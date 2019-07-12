0019de047d6f071f9b4f56d1b588e1ef39d9f042
ssombat <ssombat@users.noreply.github.com>
2019-07-12 15:21:54 -0400

    Add files via upload
    These are custom renders for xamarin forms android to pull custom tiles from a tile server

---
+++ CustomMapRender.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Maps;
+using Maps.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Maps.Android;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomMap), typeof(CustomMapRender))]
+namespace Maps.Droid
+{
+    class CustomMapRender : MapRenderer, IOnMapReadyCallback
+    {
+        const string Host = "https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png";
+        CustomMap customMap;
+        GoogleMap nativeMap;
+        private TileOverlay tileOverlay;
+        public CustomMapRender(Context context) : base(context) { }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Map> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                customMap = e.NewElement as CustomMap;
+                ((MapView)Control).GetMapAsync(this);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            UpdateTiles();
+        }
+
+        private void UpdateTiles()
+        {
+            if (nativeMap != null)
+            {
+                if (tileOverlay != null)
+                {
+                    this.tileOverlay.Remove();
+                    this.nativeMap.MapType = GoogleMap.MapTypeNormal;
+                }
+                nativeMap.MapType = GoogleMap.MapTypeNone;
+                tileOverlay = nativeMap.AddTileOverlay(new TileOverlayOptions().InvokeTileProvider(new WebTileProvider(Host)));
+
+            }
+        }
+
+        protected override void OnMapReady(GoogleMap googleMap)
+        {
+            nativeMap = googleMap;
+            UpdateTiles();
+        }
+
+    }
+}
---
+++ WebTileProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Gms.Maps.Model;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Net;
+
+namespace Maps.Droid
+{
+    public class WebTileProvider : UrlTileProvider
+    {
+        private string _url;
+        public WebTileProvider(string tileUrl) : base(256, 256)
+        {
+            _url = tileUrl;
+            Console.WriteLine("url " + _url);
+        }
+
+        public override URL GetTileUrl(int x, int y, int z)
+        {
+            var url = _url.Replace("{z}", z.ToString()).Replace("{x}", x.ToString()).Replace("{y}", y.ToString());
+            Console.WriteLine("testttttttttttt");
+            return new URL(url);
+        }
+    }
+}
